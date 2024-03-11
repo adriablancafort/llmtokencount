2b03d0797260f85b240399159ffb772501818c42
Adria Blancafort <adriablancafort@gmail.com>
2024-03-11 10:52:56 +0100

    banner event listener

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-function updateCount(){updateCharacterCount(),updateTokenCount(),updateWordCount()}function updateWordCount(){var t=document.getElementById("text-box").value.split(" ").length;document.getElementById("word-count").innerHTML=t}function updateCharacterCount(){var t=document.getElementById("text-box").value.trim().replace(/\s+/g," ").length;document.getElementById("character-count").innerHTML=t}function updateTokenCount(){var t=parseInt(document.getElementById("text-box").value.trim().replace(/\s+/g," ").length/4);document.getElementById("token-count").innerHTML=t}
+function updateCount() {
+  updateCharacterCount(), updateTokenCount(), updateWordCount();
+}
+function updateWordCount() {
+  var text = document.getElementById("text-box").value.split(" ").length;
+  document.getElementById("word-count").innerHTML = text;
+}
+function updateCharacterCount() {
+  var text = document
+    .getElementById("text-box")
+    .value.trim()
+    .replace(/\s+/g, " ").length;
+  document.getElementById("character-count").innerHTML = text;
+}
+function updateTokenCount() {
+  var text = parseInt(
+    document.getElementById("text-box").value.trim().replace(/\s+/g, " ")
+      .length / 4
+  );
+  document.getElementById("token-count").innerHTML = text;
+}
+
+var banners = document.querySelectorAll(".banner");
+
+banners.forEach(function (banner) {
+  banner.addEventListener("click", function () {
+    window.open("https://getshopgpt.com", "_blank");
+  });
+});
